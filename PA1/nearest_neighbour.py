e1e7d7335b7de9f2b9c6b68d8274f6f3072b755e
Kristoffer  Gjerde <kristoffergjerde@gmail.com>
2020-01-29 14:40:21 -0800

    All stats added

--- PA1/nearest_neighbour.py
+++ PA1/nearest_neighbour.py
@@ -1,6 +1,7 @@
 import os
 import gzip
 import numpy as np
+import scipy.stats as st
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
@@ -38,14 +39,14 @@ def uniform_random_selection(Xtrain, ytrain, M):
     return Xtrain[indices], ytrain[indices]
 
 
-def get_model(Xtrain, ytrain, algorithm='auto', metric='minkowski', n_neighbors=1, p=2):
+def get_model(Xtrain, ytrain, algorithm='auto', metric='euclidean', n_neighbors=1):
     """
     Train a KNeighborsClassifier
     :param Xtrain: Training images for the model
     :param ytrain: Labels for the images the model is "trained" on
     :return: KNeighborsClassifier
     """
-    model = KNeighborsClassifier(algorithm=algorithm, metric=metric, n_neighbors=n_neighbors, p=p)
+    model = KNeighborsClassifier(algorithm=algorithm, metric=metric, n_neighbors=n_neighbors)
     return model.fit(Xtrain, ytrain)
 
 
@@ -62,22 +63,34 @@ def test(model, Xtest, ytest):
     return error_rate
 
 
+def get_stats(error_rates):
+    mean = np.mean(error_rates)
+    std = np.std(error_rates)
+    maxi = np.max(error_rates)
+    mini = np.min(error_rates)
+    confidence_interval = st.t.interval(0.95, len(error_rates) - 1, loc=mean, scale=st.sem(error_rates))
+    print("MEAN: {} \nSTD: {} \nMAX: {} \nMIN: {} \nCONF_INT: {}".format(mean, std, maxi, mini, confidence_interval))
+    return mean, std, maxi, mini, confidence_interval
+
+
 if __name__ == '__main__':
+    # Settings for model
     ALGORITHM = "kd_tree"  # Algorithm used to make model more efficient
-    METRIC = "minkowski"  # Distance metric in the model
-    P = 2  # When p=2 minkowski is equal to Euclidean distance
+    METRIC = "euclidean"  # Distance metric in the model
     N_NEIGHBORS = 1  # Number of neighbors in k-NN (Assignments ask for 1-NN, so use k=1)
 
-    M = 10000  # Number of images to sample
+    # Settings for prototyping
+    M = 1000  # Number of images to sample
     N_COMPONENTS = 40  # Number of components to use for the PCA
+    K = 5  # Number of folds
 
     # Load data
     Xtrain, ytrain = load_data("./", mode="train")
     Xtest, ytest = load_data("./", mode="t10k")
 
-    k = 5
+    error_rates = []  # List of error rates for each fold
     # In order to get error bars we need to do the prototyping k times
-    for fold in range(k):
+    for fold in range(1, K + 1):
         # Do prototyping
         Xtrain_rand, ytrain_rand = uniform_random_selection(Xtrain, ytrain, M)
 
@@ -87,8 +100,11 @@ if __name__ == '__main__':
         Xtest_transformed = pca.transform(Xtest)
 
         # Initialize a model
-        model = get_model(Xtrain_rand, ytrain_rand, algorithm=ALGORITHM, metric=METRIC, n_neighbors=N_NEIGHBORS, p=P)
+        model = get_model(Xtrain_rand, ytrain_rand, algorithm=ALGORITHM, metric=METRIC, n_neighbors=N_NEIGHBORS)
 
         # Test model
         error_rate = test(model, Xtest_transformed, ytest)
+        error_rates.append(error_rate)
         print("FOLD: {}, ERROR RATE: {}".format(fold, error_rate))
+
+    get_stats(error_rates)
